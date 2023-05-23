48050875d0b5c71816776d93f2a32894ef163a72
Alexander Andriets <oleksandr.andriiets@halliburton.com>
2023-05-23 20:00:48 +0200

    adjust comments

--- api/comments/serializers.py
+++ api/comments/serializers.py
@@ -6,6 +6,7 @@ class CommentsSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.email')
     updated_by = serializers.ReadOnlyField(source='updated_by.email')
     lecture = serializers.CharField(source='lecture_id')
+    course = serializers.ReadOnlyField(source='lecture.course.id')
 
     class Meta:
         model = Comments
@@ -14,9 +15,10 @@ class CommentsSerializer(serializers.HyperlinkedModelSerializer):
             'id',
             'comment',
             'lecture',
+            'course',
             'owner',
             'created_at',
             'updated_at',
             'updated_by',
         ]
-        read_only_fields = ['id', 'owner', 'created_at', 'updated_at', 'updated_by']
+        read_only_fields = ['id', 'course', 'owner', 'created_at', 'updated_at', 'updated_by']
